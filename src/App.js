6c396d0e2967182d65c5ac4b69d0fa0737c6861f
deepak kumar <deepak.kumar13@oyorooms.com>
2021-06-02 20:52:28 +0530

    test

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Detail from './components/detail/Detail'
 function App() {
   return (
     <div className="App">
+      <Welcome></Welcome>
       <BrowserRouter>
         <Switch>
             <Route path="/" exact component={Welcome} />
